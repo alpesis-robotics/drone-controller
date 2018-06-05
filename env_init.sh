12df3620dc04cd4835e1c2ed8b1ee10ea75d4553
K <kwailamchan@hotmail.com>
2018-06-05 16:23:52 +0800

    [master] reinitialized and modified mass param in QuadControlParams. 	modified:   .gitignore 	deleted:    Instruction.md 	new file:   README.md 	deleted:    README.rst 	modified:   config/QuadControlParams.txt 	new file:   env_init.sh

---
+++ env_init.sh
@@ -0,0 +1,4 @@
+# install Qt5
+export Qt5Core_DIR=/usr/local/opt/qt/lib/cmake/Qt5Core
+export Qt5Network_DIR=/usr/local/opt/qt/lib/cmake/Qt5Network
+export Qt5Widgets_DIR=/usr/local/opt/qt/lib/cmake/Qt5Widgets
